b2ba7045b63ab997fd611a397d11118e645fcc67
MateuszPospiech <pospiechmateusz@gmail.com>
2019-05-05 23:20:29 +0200

    Added state on form

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -2,25 +2,89 @@ import React from 'react';
 
 class AdminPanel extends React.Component {
     
+    constructor() {
+        super();
+        this.state = {
+            book : {
+                name : "",
+                author: "",
+                description: "",
+                onStock: false,
+                image: ""
+            },
+            books : []
+        }
+    };
+
+    handleChange = (event) => {
+
+        let newBook;
+
+        if(event.target.name==="onStock") {
+            newBook = {
+                ...this.state.book,
+                [event.target.name]: event.target.checked
+            };
+        } else {
+            newBook = {
+                ...this.state.book,
+                [event.target.name]: event.target.value
+            };
+        }
+        
+        
+        this.setState({
+            book: newBook
+        });
+    }
+
+    addNewBook = (event) => {
+
+        event.preventDefault(); //do not reload 
+
+        let newBooks = [...this.state.books]; //split all books into book copy of all books
+
+        let newBook = {...this.state.book}; //split book into propertys copy of all propertys
+
+        newBooks.push(newBook);
+
+        this.setState({
+            books : newBooks,
+            book : {
+                name : "",
+                author: "",
+                description: "",
+                onStock: true,
+                image: ""
+            }
+        });
+
+    }
+
     render() {
         return (
             <div className="adminPanel col-4 col-xs-12">
-                <form>
+                <form onSubmit={this.addNewBook}>
                     <div className="form-group">
-                        <input type="text" placeholder="Book name" id="bookName" name="bookName" className="form-control"></input>
+                        <input type="text" placeholder="Book name" id="name" name="name" className="form-control" 
+                        onChange={this.handleChange} value={this.state.book.name}></input>
                     </div>
                     <div className="form-group">
-                        <input type="text" placeholder="Book author" id="bookAuthor" name="bookAuthor" className="form-control"></input>
+                        <input type="text" placeholder="Book author" id="author" name="author" className="form-control"
+                        onChange={this.handleChange} value={this.state.book.author}></input>
                     </div>
                     <div className="form-group">
-                        <textarea placeholder="Book description" id="bookDescription" name="bookDescription" className="form-control"></textarea>
+                        <textarea placeholder="Book description" id="description" name="description" className="form-control"
+                        onChange={this.handleChange} value={this.state.book.description}></textarea>
                     </div>
                     <div className="form-group">
-                        <input type="checkbox" id="bookOnStock" name="bookOnStock" className=""></input>
-                        <label htmlFor="bookOnStock" className="form-chech-label">On stock</label>
+                        <input type="checkbox" id="onStock" name="onStock" 
+                        onChange={this.handleChange} value={this.state.book.onStock}></input>
+                        <label htmlFor="onStock" className="form-chech-label">On stock</label>
                     </div>
                     <div className="form-group">
-                        <input type="text" placeholder="Book image" id="bookImage" name="bookImage" className="form-control"></input>
+                        <input type="text" placeholder="Book image" id="image" name="image" className="form-control"
+                        onChange={this.handleChange} value={this.state.book.image}></input>
                     </div>
                     <button type="submit" className="btn btn-primary">Add</button>
                 </form>
